ec347b460b1a74412b7e0f4f861484276313b869
bmassioui <bouchaib.massioui@gmail.com>
2022-04-02 10:44:41 +0100

    create problem solving class

---
+++ dotnet_problem_solving/ProblemSolving.cs
@@ -0,0 +1,9 @@
+namespace dotnet_problem_solving;
+
+/// <summary>
+/// Problem Solution
+/// </summary>
+public class ProblemSolving
+{
+
+}
---
+++ dotnet_problem_solving_tdd/ProblemSolvingUnitTest.cs
@@ -0,0 +1,20 @@
+using dotnet_problem_solving;
+using Xunit;
+
+namespace dotnet_problem_solving_tdd;
+
+public class ProblemSolvingUnitTest
+{
+    private readonly ProblemSolving _problemSolving;
+
+    public ProblemSolvingUnitTest()
+    {
+        _problemSolving = new ProblemSolving();
+    }
+
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
