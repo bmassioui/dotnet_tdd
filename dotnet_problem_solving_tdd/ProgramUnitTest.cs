0eaf291f1a4afbf4e5b40bc19712c8bd606ea9c1
bmassioui <bouchaib.massioui@gmail.com>
2022-04-02 10:41:22 +0100

    add tdd project reference

--- dotnet_problem_solving_tdd/ProgramUnitTest.cs
+++ dotnet_problem_solving_tdd/ProgramUnitTest.cs
@@ -2,8 +2,13 @@ using Xunit;
 
 namespace dotnet_problem_solving_tdd;
 
-public class UnitTest1
+public class ProgramUnitTest
 {
+    public ProgramUnitTest()
+    {
+        
+    } 
+
     [Fact]
     public void Test1()
     {
